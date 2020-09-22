7eb839bffab5e3f45f18d15ea875ec7490c5b845
sretree9 <sretree9@gmail.com>
2020-09-22 10:06:02 +0800

    '0.0.1'

--- src/router/routes.js
+++
@@ -1,7 +0,0 @@
-export const routes = [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: () => import('@/page/HelloWorld')
-    }
-  ]
--- vue-axios-ellement-/src/element_ui/index.js
+++
@@ -1,8 +0,0 @@
-// 导入自己需要的组件
-import { Select } from 'element-ui'
-const element = {
-  install: function (Vue) {
-    Vue.use(Select)
-  }
-}
-export default element
